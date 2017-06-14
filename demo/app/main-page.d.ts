06e37cd7d7254fcba2fc4cfc160d7bbf0f5a255c
Osei Fortune <fortune.osei@yahoo.com>
2017-06-14 14:51:38 -0400

    Clean up & package fix

--- demo/app/main-page.d.ts
+++
@@ -1,3 +0,0 @@
-import * as observable from 'data/observable';
-export declare function pageLoaded(args: observable.EventData): void;
-export declare function addImage(): void;
--- demo/app/main-view-model.d.ts
+++
@@ -1,7 +0,0 @@
-import { Observable } from 'data/observable';
-export declare class HelloWorldModel extends Observable {
-    images: any;
-    newImg: string;
-    constructor();
-    addImage(): void;
-}
